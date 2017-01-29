314b834665987e6e57ddd78ec2c1122474cc8a28
Pape Ndiaye <papeballe@gmail.com>
2017-01-29 22:05:34 +0100

    #Master base project

---
+++ app/src/main/java/upem/univ/fr/amcf/HomeActiviry.java
@@ -0,0 +1,13 @@
+package upem.univ.fr.amcf;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class HomeActiviry extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home_activiry);
+    }
+}
