7328e288986a912ad18594cceebab5659a5a40db
Pape Ndiaye <papeballe@gmail.com>
2017-02-03 00:44:15 +0100

    #SMSBroadcastReceiver Login done

--- app/src/main/java/upem/univ/fr/amcf/HomeActiviry.java
+++ app/src/main/java/upem/univ/fr/amcf/HomeActiviry.java
@@ -1,23 +1,24 @@
 package upem.univ.fr.amcf;
 
-import android.app.NotificationManager;
-import android.app.PendingIntent;
-import android.content.Context;
+
 import android.content.Intent;
-import android.net.Uri;
-import android.support.v4.app.NotificationCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import com.facebook.FacebookSdk;
+import com.facebook.appevents.AppEventsLogger;
+import com.facebook.login.LoginManager;
+import com.facebook.login.widget.LoginButton;
 
 public class HomeActiviry extends AppCompatActivity {
-
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_activiry);
+        FacebookSdk.setApplicationId("1046614502109093");
+        FacebookSdk.sdkInitialize(getApplicationContext());
+        AppEventsLogger.activateApp(this);
     }
 
     public void startSendSMSActivity(View v) {
@@ -27,7 +28,14 @@ public class HomeActiviry extends AppCompatActivity {
     public void startListSMSActivity(View v) {
         startActivity(new Intent(HomeActiviry.this, SMSList.class));
     }
-
+    public void goBackToLoginScreen(View v) {
+        Intent intent = new Intent(this, LoginFacebookActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(intent);
+    }
+    public void logoutFacebookButtonClick (View v){
+        LoginManager.getInstance().logOut();
+    }
 
 
 }
