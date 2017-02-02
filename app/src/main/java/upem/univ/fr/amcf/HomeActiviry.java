1c39333a23211354583aff2ba51b578527d854b3
Pape Ndiaye <papeballe@gmail.com>
2017-02-02 22:51:49 +0100

    #SMSBroadcastReceiver SMS DONE

--- app/src/main/java/upem/univ/fr/amcf/HomeActiviry.java
+++ app/src/main/java/upem/univ/fr/amcf/HomeActiviry.java
@@ -1,13 +1,33 @@
 package upem.univ.fr.amcf;
 
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v4.app.NotificationCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class HomeActiviry extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_activiry);
     }
+
+    public void startSendSMSActivity(View v) {
+        startActivity(new Intent(HomeActiviry.this, SMSSender.class));
+    }
+
+    public void startListSMSActivity(View v) {
+        startActivity(new Intent(HomeActiviry.this, SMSList.class));
+    }
+
+
+
 }
