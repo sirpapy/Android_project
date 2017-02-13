6aae6d8ae393a7a79f4755c76f35df01ed21a4c1
Damien CHESNEAU <damien.chesneau@nokia.com>
2017-02-13 19:02:05 +0100

    Contact view by using a grid

--- amcf/src/main/java/fr/amcf/ContactActivity.java
+++ amcf/src/main/java/fr/amcf/ContactActivity.java
@@ -1,37 +1,57 @@
 package fr.amcf;
 
-import android.Manifest;
+import android.app.Activity;
 import android.content.ContentResolver;
 import android.content.pm.PackageManager;
 import android.database.Cursor;
 import android.os.Bundle;
 import android.provider.ContactsContract;
-import android.support.v4.app.ActivityCompat;
-import android.support.v4.content.ContextCompat;
+import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.ArrayAdapter;
-import android.widget.ListView;
 import android.widget.Toast;
 
 import java.util.ArrayList;
+import java.util.List;
 
-public class ContactActivity extends AppCompatActivity {
-    private ListView listcontact = (ListView) findViewById(R.id.listcontact);
+public class ContactActivity extends Activity {
+    private GridLayoutManager lLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contact);
-        Toast.makeText(this, "Coucou", Toast.LENGTH_LONG);
-        if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_CONTACTS) != PackageManager.PERMISSION_GRANTED) {
+
+        List<ItemObject> rowListItem = getAllItemList();
+        lLayout = new GridLayoutManager(this, 4);
+
+        RecyclerView rView = (RecyclerView) findViewById(R.id.recycler_view);
+        rView.setHasFixedSize(true);
+        rView.setLayoutManager(lLayout);
+
+        RecyclerViewAdapter rcAdapter = new RecyclerViewAdapter(ContactActivity.this, rowListItem);
+        rView.setAdapter(rcAdapter);
+
+        //((MyRecyclerViewAdapter) mAdapter).addItem(obj, index);
+
+        // Code to remove an item with default animation
+        //((MyRecyclerViewAdapter) mAdapter).deleteItem(index);
+//        ArrayAdapter adapter = new ArrayAdapter<>(getApplicationContext(), android.R.layout.simple_spinner_item, Arrays.asList("Toto", "tata"));
+//
+//        listcontact.setAdapter(adapter);
+//        Toast.makeText(this, "Coucou", Toast.LENGTH_LONG);
+       /* if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_CONTACTS) != PackageManager.PERMISSION_GRANTED) {
             if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.READ_CONTACTS)) {
             } else {
                 ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_CONTACTS}, 2);
             }
         } else {
             initContacts();
-        }
+        }*/
     }
 
     @Override
@@ -76,16 +96,67 @@ public class ContactActivity extends AppCompatActivity {
             }
             ArrayAdapter adapter = new ArrayAdapter<>(getApplicationContext(), android.R.layout.simple_spinner_item, elementsToPrint);
 
-            listcontact.setAdapter(adapter);
+//            listcontact.setAdapter(adapter);
+//
+//            listcontact.setOnClickListener(new View.OnClickListener() {
+//                @Override
+//                public void onClick(View view) {
 
-            listcontact.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View view) {
-
-                    //arrayList.add(editTxt.getText().toString());
-                    //adapter.notifyDataSetChanged();
-                }
-            });
+            //arrayList.add(editTxt.getText().toString());
+            //adapter.notifyDataSetChanged();
+//                }
+//            });
         }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        if (id == R.id.action_refresh) {
+            Toast.makeText(ContactActivity.this, "Refresh App", Toast.LENGTH_LONG).show();
+        }
+        if (id == R.id.action_new) {
+            Toast.makeText(ContactActivity.this, "Create Text", Toast.LENGTH_LONG).show();
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    private List<ItemObject> getAllItemList() {
+
+        List<ItemObject> allItems = new ArrayList<>();
+        allItems.add(new ItemObject("United States", R.drawable.one));
+        allItems.add(new ItemObject("Canada", R.drawable.two));
+        allItems.add(new ItemObject("United Kingdom", R.drawable.three));
+        allItems.add(new ItemObject("Germany", R.drawable.four));
+        allItems.add(new ItemObject("Sweden", R.drawable.five));
+        allItems.add(new ItemObject("United Kingdom", R.drawable.six));
+        allItems.add(new ItemObject("Germany", R.drawable.seven));
+        allItems.add(new ItemObject("Sweden", R.drawable.eight));
+        allItems.add(new ItemObject("United States", R.drawable.one));
+        allItems.add(new ItemObject("Canada", R.drawable.two));
+        allItems.add(new ItemObject("United Kingdom", R.drawable.three));
+        allItems.add(new ItemObject("Germany", R.drawable.four));
+        allItems.add(new ItemObject("Sweden", R.drawable.five));
+        allItems.add(new ItemObject("United Kingdom", R.drawable.six));
+        allItems.add(new ItemObject("Germany", R.drawable.seven));
+        allItems.add(new ItemObject("Sweden", R.drawable.eight));
+
+        return allItems;
     }
 }
