2dcef248c95900472062ac8229307db822a17c08
Damien CHESNEAU <damien.chesneau@nokia.com>
2017-02-13 22:51:15 +0100

    Recyclable view for contacts finish

--- amcf/src/main/java/fr/amcf/ContactActivity.java
+++ amcf/src/main/java/fr/amcf/ContactActivity.java
@@ -1,162 +1,65 @@
 package fr.amcf;
 
-import android.app.Activity;
-import android.content.ContentResolver;
-import android.content.pm.PackageManager;
-import android.database.Cursor;
 import android.os.Bundle;
-import android.provider.ContactsContract;
-import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.widget.ArrayAdapter;
+import android.view.View;
 import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class ContactActivity extends Activity {
-    private GridLayoutManager lLayout;
+/**
+ * Created by dchesnea on 15/02/2017.
+ */
+public class ContactActivity extends AppCompatActivity {
+    private List<Contact> contactList = new ArrayList<>();
+    private RecyclerView recyclerView;
+    private ContactsAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_contact);
+        setContentView(R.layout.contact_activity);
 
-        List<ItemObject> rowListItem = getAllItemList();
-        lLayout = new GridLayoutManager(this, 4);
+        recyclerView = (RecyclerView) findViewById(R.id.recycler_view);
 
-        RecyclerView rView = (RecyclerView) findViewById(R.id.recycler_view);
-        rView.setHasFixedSize(true);
-        rView.setLayoutManager(lLayout);
+        adapter = new ContactsAdapter(contactList);
 
-        RecyclerViewAdapter rcAdapter = new RecyclerViewAdapter(ContactActivity.this, rowListItem);
-        rView.setAdapter(rcAdapter);
+        recyclerView.setHasFixedSize(true);
+        RecyclerView.LayoutManager mLayoutManager = new LinearLayoutManager(getApplicationContext());
+        recyclerView.setLayoutManager(mLayoutManager);
+        recyclerView.addItemDecoration(new DividerItemDecoration(this, LinearLayoutManager.VERTICAL));
+        recyclerView.setItemAnimator(new DefaultItemAnimator());
+        recyclerView.setAdapter(adapter);
 
-        //((MyRecyclerViewAdapter) mAdapter).addItem(obj, index);
-
-        // Code to remove an item with default animation
-        //((MyRecyclerViewAdapter) mAdapter).deleteItem(index);
-//        ArrayAdapter adapter = new ArrayAdapter<>(getApplicationContext(), android.R.layout.simple_spinner_item, Arrays.asList("Toto", "tata"));
-//
-//        listcontact.setAdapter(adapter);
-//        Toast.makeText(this, "Coucou", Toast.LENGTH_LONG);
-       /* if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_CONTACTS) != PackageManager.PERMISSION_GRANTED) {
-            if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.READ_CONTACTS)) {
-            } else {
-                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_CONTACTS}, 2);
+        recyclerView.addOnItemTouchListener(new RecyclerTouchListener(getApplicationContext(), recyclerView, new RecyclerTouchListener.ClickListener() {
+            @Override
+            public void onClick(View view, int position) {
+                Contact contact = contactList.get(position);
+                Toast.makeText(getApplicationContext(), contact.getName() + " selected :)", Toast.LENGTH_SHORT).show();
             }
-        } else {
-            initContacts();
-        }*/
-    }
 
-    @Override
-    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
-        switch (requestCode) {
-            case 2: {
-                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                    initContacts();
-                } else {
-                    //TODO
-                }
-                return;
+            @Override
+            public void onLongClick(View view, int position) {
+                //TODO
             }
-        }
+        }));
+
+        prepareMovieData();
     }
 
-    private void initContacts() {
-        ContentResolver cr = getContentResolver();
-        Cursor cur = cr.query(ContactsContract.Contacts.CONTENT_URI, null, null, null, null);
-        if (cur.getCount() > 0) {
-            ArrayList<Contact> builders = new ArrayList<>();
-            while (cur.moveToNext()) {
-                String id = cur.getString(cur.getColumnIndex(ContactsContract.Contacts._ID));
-                String name = cur.getString(cur.getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME));
-                String email = "TODO";// cur.getString(cur.getColumnIndex(ContactsContract.CommonDataKinds.Email.ADDRESS));
-
-                if (cur.getInt(cur.getColumnIndex(ContactsContract.Contacts.HAS_PHONE_NUMBER)) > 0) {
-                    Cursor pCur = cr.query(ContactsContract.CommonDataKinds.Phone.CONTENT_URI,
-                            null, ContactsContract.CommonDataKinds.Phone.CONTACT_ID + " = ?", new String[]{id}, null);
-                    while (pCur.moveToNext()) {
-                        String phoneNo = pCur.getString(pCur.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER));
-                        builders.add(new Contact.Builder().setName(name)
-                                .setEmail(email).setPrimaryPhoneNumber(phoneNo).build());
-                    }
-                    pCur.close();
-                }
-            }
-            Toast.makeText(this, builders.get(0).toString(), Toast.LENGTH_SHORT).show();
-            ArrayList<String> elementsToPrint = new ArrayList<>();
-            for (Contact contact : builders) {
-                elementsToPrint.add(contact.toString());
-            }
-            ArrayAdapter adapter = new ArrayAdapter<>(getApplicationContext(), android.R.layout.simple_spinner_item, elementsToPrint);
-
-//            listcontact.setAdapter(adapter);
-//
-//            listcontact.setOnClickListener(new View.OnClickListener() {
-//                @Override
-//                public void onClick(View view) {
-
-            //arrayList.add(editTxt.getText().toString());
-            //adapter.notifyDataSetChanged();
-//                }
-//            });
-        }
+    private void prepareMovieData() {
+        contactList.add(new Contact.Builder().setEmail("aa@gmail.com").setName("Damien Chesneau").setPrimaryPhoneNumber("+33638735700").build());
+        contactList.add(new Contact.Builder().setEmail("aa@gmail.com").setName("Damien Chesneau").setPrimaryPhoneNumber("+33638735700").build());
+        contactList.add(new Contact.Builder().setEmail("aa@gmail.com").setName("Damien Chesneau").setPrimaryPhoneNumber("+33638735700").build());
+        contactList.add(new Contact.Builder().setEmail("aa@gmail.com").setName("Damien Chesneau").setPrimaryPhoneNumber("+33638735700").build());
+        contactList.add(new Contact.Builder().setEmail("aa@gmail.com").setName("Damien Chesneau").setPrimaryPhoneNumber("+33638735700").build());
+        contactList.add(new Contact.Builder().setEmail("aa@gmail.com").setName("Damien Chesneau").setPrimaryPhoneNumber("+33638735700").build());
+        contactList.add(new Contact.Builder().setEmail("aa@gmail.com").setName("Damien Chesneau").setPrimaryPhoneNumber("+33638735700").build());
+        adapter.notifyDataSetChanged();
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        if (id == R.id.action_refresh) {
-            Toast.makeText(ContactActivity.this, "Refresh App", Toast.LENGTH_LONG).show();
-        }
-        if (id == R.id.action_new) {
-            Toast.makeText(ContactActivity.this, "Create Text", Toast.LENGTH_LONG).show();
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
-    private List<ItemObject> getAllItemList() {
-
-        List<ItemObject> allItems = new ArrayList<>();
-        allItems.add(new ItemObject("United States", R.drawable.one));
-        allItems.add(new ItemObject("Canada", R.drawable.two));
-        allItems.add(new ItemObject("United Kingdom", R.drawable.three));
-        allItems.add(new ItemObject("Germany", R.drawable.four));
-        allItems.add(new ItemObject("Sweden", R.drawable.five));
-        allItems.add(new ItemObject("United Kingdom", R.drawable.six));
-        allItems.add(new ItemObject("Germany", R.drawable.seven));
-        allItems.add(new ItemObject("Sweden", R.drawable.eight));
-        allItems.add(new ItemObject("United States", R.drawable.one));
-        allItems.add(new ItemObject("Canada", R.drawable.two));
-        allItems.add(new ItemObject("United Kingdom", R.drawable.three));
-        allItems.add(new ItemObject("Germany", R.drawable.four));
-        allItems.add(new ItemObject("Sweden", R.drawable.five));
-        allItems.add(new ItemObject("United Kingdom", R.drawable.six));
-        allItems.add(new ItemObject("Germany", R.drawable.seven));
-        allItems.add(new ItemObject("Sweden", R.drawable.eight));
-
-        return allItems;
-    }
 }
--- amcf/src/main/java/fr/amcf/DividerItemDecoration.java
+++ amcf/src/main/java/fr/amcf/DividerItemDecoration.java
@@ -9,18 +9,21 @@ import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
 
+/**
+ * Created by dchesnea on 15/02/2017.
+ */
 public class DividerItemDecoration extends RecyclerView.ItemDecoration {
-    private static final int[] ATTRS = new int[]{
-            android.R.attr.listDivider
-    };
+
     public static final int HORIZONTAL_LIST = LinearLayoutManager.HORIZONTAL;
     public static final int VERTICAL_LIST = LinearLayoutManager.VERTICAL;
-    private Drawable mDivider;
-    private int mOrientation;
+    private static final int[] ATTRS = new int[]{android.R.attr.listDivider};
+
+    private int orientation;
+    private Drawable divider;
 
     public DividerItemDecoration(Context context, int orientation) {
         final TypedArray a = context.obtainStyledAttributes(ATTRS);
-        mDivider = a.getDrawable(0);
+        divider = a.getDrawable(0);
         a.recycle();
         setOrientation(orientation);
     }
@@ -29,12 +32,12 @@ public class DividerItemDecoration extends RecyclerView.ItemDecoration {
         if (orientation != HORIZONTAL_LIST && orientation != VERTICAL_LIST) {
             throw new IllegalArgumentException("invalid orientation");
         }
-        mOrientation = orientation;
+        this.orientation = orientation;
     }
 
     @Override
-    public void onDraw(Canvas c, RecyclerView parent) {
-        if (mOrientation == VERTICAL_LIST) {
+    public void onDrawOver(Canvas c, RecyclerView parent, RecyclerView.State state) {
+        if (orientation == VERTICAL_LIST) {
             drawVertical(c, parent);
         } else {
             drawHorizontal(c, parent);
@@ -42,41 +45,42 @@ public class DividerItemDecoration extends RecyclerView.ItemDecoration {
     }
 
     public void drawVertical(Canvas c, RecyclerView parent) {
-        final int left = parent.getPaddingLeft();
-        final int right = parent.getWidth() - parent.getPaddingRight();
-        final int childCount = parent.getChildCount();
+        int left = parent.getPaddingLeft();
+        int right = parent.getWidth() - parent.getPaddingRight();
+        int childCount = parent.getChildCount();
         for (int i = 0; i < childCount; i++) {
-            final View child = parent.getChildAt(i);
-            final RecyclerView.LayoutParams params = (RecyclerView.LayoutParams) child
+            View child = parent.getChildAt(i);
+            RecyclerView.LayoutParams params = (RecyclerView.LayoutParams) child
                     .getLayoutParams();
-            final int top = child.getBottom() + params.bottomMargin;
-            final int bottom = top + mDivider.getIntrinsicHeight();
-            mDivider.setBounds(left, top, right, bottom);
-            mDivider.draw(c);
+            int top = child.getBottom() + params.bottomMargin;
+            int bottom = top + divider.getIntrinsicHeight();
+            divider.setBounds(left, top, right, bottom);
+            divider.draw(c);
         }
     }
 
     public void drawHorizontal(Canvas c, RecyclerView parent) {
-        final int top = parent.getPaddingTop();
-        final int bottom = parent.getHeight() - parent.getPaddingBottom();
-        final int childCount = parent.getChildCount();
+        int top = parent.getPaddingTop();
+        int bottom = parent.getHeight() - parent.getPaddingBottom();
+
+        int childCount = parent.getChildCount();
         for (int i = 0; i < childCount; i++) {
-            final View child = parent.getChildAt(i);
-            final RecyclerView.LayoutParams params = (RecyclerView.LayoutParams) child
+            View child = parent.getChildAt(i);
+            RecyclerView.LayoutParams params = (RecyclerView.LayoutParams) child
                     .getLayoutParams();
-            final int left = child.getRight() + params.rightMargin;
-            final int right = left + mDivider.getIntrinsicHeight();
-            mDivider.setBounds(left, top, right, bottom);
-            mDivider.draw(c);
+            int left = child.getRight() + params.rightMargin;
+            int right = left + divider.getIntrinsicHeight();
+            divider.setBounds(left, top, right, bottom);
+            divider.draw(c);
         }
     }
 
     @Override
-    public void getItemOffsets(Rect outRect, int itemPosition, RecyclerView parent) {
-        if (mOrientation == VERTICAL_LIST) {
-            outRect.set(0, 0, 0, mDivider.getIntrinsicHeight());
+    public void getItemOffsets(Rect outRect, View view, RecyclerView parent, RecyclerView.State state) {
+        if (orientation == VERTICAL_LIST) {
+            outRect.set(0, 0, 0, divider.getIntrinsicHeight());
         } else {
-            outRect.set(0, 0, mDivider.getIntrinsicWidth(), 0);
+            outRect.set(0, 0, divider.getIntrinsicWidth(), 0);
         }
     }
 }
