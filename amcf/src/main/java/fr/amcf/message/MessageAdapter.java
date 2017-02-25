e3b92c3544553d4dc7406b4b814c590542d40d3f
frNico <frechet.nicolas@gmail.com>
2017-02-25 22:27:44 +0100

    #work display conversation in progress

--- amcf/src/main/java/fr/amcf/message/MessageAdapter.java
+++ amcf/src/main/java/fr/amcf/message/MessageAdapter.java
@@ -1,9 +1,15 @@
 package fr.amcf.message;
 
 import android.app.Activity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.TextView;
 
 import java.util.List;
+
+import fr.amcf.R;
 
 /**
  * Created by frech on 25/02/2017.
@@ -20,5 +26,53 @@ public class MessageAdapter extends ArrayAdapter<Message> {
         this.messages = messages;
     }
 
-    
+   /* @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        ViewHolder holder;
+        LayoutInflater inflater = (LayoutInflater) activity.getSystemService(Activity.LAYOUT_INFLATER_SERVICE);
+
+        int layoutResource = 0; // determined by view type
+        Message message = getItem(position);
+        int viewType = getItemViewType(position);
+
+        if (message.isMine()) {
+            layoutResource = R.layout.item_chat_left;
+        } else {
+            layoutResource = R.layout.item_chat_right;
+        }
+
+        if (convertView != null) {
+            holder = (ViewHolder) convertView.getTag();
+        } else {
+            convertView = inflater.inflate(layoutResource, parent, false);
+            holder = new ViewHolder(convertView);
+            convertView.setTag(holder);
+        }
+
+        //set message content
+        holder.msg.setText(message.getBody());
+
+        return convertView;
+    }
+
+    @Override
+    public int getViewTypeCount() {
+        // return the total number of view types. this value should never change
+        // at runtime
+        return 2;
+    }
+
+    @Override
+    public int getItemViewType(int position) {
+        // return a value between 0 and (getViewTypeCount - 1)
+        return position % 2;
+    }
+
+    private class ViewHolder {
+        private TextView msg;
+
+        public ViewHolder(View v) {
+            msg = (TextView) v.findViewById(R.id.txt_msg);
+        }
+    }*/
 }
